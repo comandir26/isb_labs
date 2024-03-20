5f51aab9ebf3b46dc808f4f59d5833a83e21189c
Vladislav <WWolkoW04@yandex.ru>
2024-03-21 02:12:54 +0300

    1 task has been completed, but the substitution algorithm needs to be corrected, since there are incorrect values in the encryption key

---
+++ lab_1/task_1/encryption.py
@@ -0,0 +1,34 @@
+import json
+
+letters = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+
+
+def substitution_cipher(keyword, path_to_original, path_to_result, path_to_encryption_key) -> None:
+    numbers = range(1, len(letters) + 1)
+    data = dict(zip(letters, numbers))
+    substitutions = {}
+    with open(path_to_original, 'r', encoding='utf-8') as f:
+        text = f.read()
+    keyword = keyword * int(len(text)/len(keyword)) + \
+        keyword[:len(text) % len(keyword)]
+    result = ''
+    for text_letter, key_letter in zip(text, keyword):
+        if text_letter.upper() not in letters:
+            result += text_letter
+            continue
+        count = data.get(text_letter.upper()) + data.get(key_letter.upper())
+        if count > 32:
+            count -= 32
+        result += letters[count-1]
+        substitutions[text_letter.upper()] = letters[count-1].upper()
+    with open(path_to_result, 'w', encoding='utf-8') as f:
+        f.write(result)
+    with open(path_to_encryption_key, 'w', encoding='utf-8') as json_file:
+        json.dump(substitutions, json_file, ensure_ascii=False)
+
+
+def main() -> None:
+    with open('lab_1/task_1/settings.json', encoding='utf-8') as json_file:
+        settings = json.load(json_file)
+    substitution_cipher(settings['keyword'], settings['original'],
+                        settings['encrypted'], settings['encryption_key'])
---
+++ lab_1/task_1/main.py
@@ -0,0 +1,5 @@
+from encryption import main
+
+
+if __name__ == '__main__':
+    main()
