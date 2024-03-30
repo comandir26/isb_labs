e9600da990f10649a573aca40d60fa2368189a23
Vladislav <WWolkoW04@yandex.ru>
2024-03-30 21:42:17 +0300

    I found the encryption key and decrypted the text

--- lab_1/task_2/decryption.py
+++ lab_1/task_2/decryption.py
@@ -18,19 +18,19 @@ def frequency_analysis(path_to_encrypted, path_to_frequency):
     with open(path_to_frequency, 'w', encoding='utf-8') as json_file:
         json.dump(sorted_result, json_file, ensure_ascii=False)
 
-def decoding(path_to_encrypted, path_to_freq_encrypted, path_to_freq_alphabet, path_to_decrypted):
+def decryption(path_to_encrypted, path_to_key, path_to_decrypted):
     with open(path_to_encrypted, 'r', encoding='utf-8') as f:
         text = f.read()
-    with open(path_to_freq_encrypted, 'r', encoding='utf-8') as json_file:
-        freq_encrypted = json.load(json_file)
-    with open(path_to_freq_alphabet, 'r', encoding='utf-8') as json_file:
-        freq_alphabet = json.load(json_file)
-    for encrypt, decrypt in zip(freq_encrypted.keys(), freq_alphabet.keys()):
-        text = text.replace(encrypt, decrypt)
-        print(text)
-    
+    with open(path_to_key, 'r', encoding='utf-8') as json_file:
+        sub = json.load(json_file)
+    result = ''
+    for letter in text:
+        result+=sub.get(letter)
+    with open(path_to_decrypted, 'w', encoding='utf-8') as f:
+        f.write(result)
+
 def main() -> None:
     with open('lab_1/task_2/settings.json', 'r', encoding='utf-8') as json_file:
         settings = json.load(json_file)
     #frequency_analysis(settings['encrypted'], settings['frequency_encrypted'])
-    decoding(settings['encrypted'], settings['frequency_encrypted'], settings['frequency_alphabet'], settings['decrypted'])
+    decryption(settings['encrypted'], settings['encryption_key'], settings['decrypted'])
