f118c9251ace398e2bad378544353d4216002c52
Vladislav <WWolkoW04@yandex.ru>
2024-05-03 01:35:10 +0400

    The functions for working with files are placed in a separate module

---
+++ lab_3/files.py
@@ -0,0 +1,194 @@
+import json
+
+from typing import Optional, Dict
+
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+
+
+def read_json(path_to_data: str) -> Optional[Dict[str, str]]:
+    """
+    This function reads data in json format at the specified path.
+
+    Parameters:
+        path_to_data: str
+          The path to the data
+
+    Returns:
+        data: Optional[Dict[str, str]]
+          The read data or None
+    """
+    try:
+        with open(path_to_data, 'r') as json_file:
+            data = json.load(json_file)
+    except FileNotFoundError:
+        data = None
+    finally:
+        return data
+    
+
+def read_bytes(path_to_data: str) -> Optional[bytes]:
+    """
+    This function reads data in bytes format at the specified path, 
+    it can be the encrypted text or the encrypted symmetric key.
+
+    Parameters:
+        path_to_data: str
+          The path to the data
+
+    Returns:
+        data: bytes
+          The read data or None
+    """
+    try:
+        with open(path_to_data, 'rb') as f:
+            data = f.read()
+    except FileNotFoundError:
+        data = None
+    finally:
+        return data
+
+
+def read_text(path_to_text: str) -> Optional[str]:
+    """
+    This function reads text at the specified path.
+
+    Parameters:
+        path_to_text: str
+          The path to the text
+
+    Returns:
+        text: str
+          The read text or None
+    """
+    try:
+        with open(path_to_text, 'r', encoding='utf-8') as f:
+            text = f.read()
+    except FileNotFoundError:
+        text = None
+    finally:
+        return text
+    
+
+def save_text(path_to_save: str, text: str) -> bool:
+    """
+    This function saves text at the specified path.
+
+    Parameters:
+        path_to_save: str
+          The path to save the text
+        text: str
+          The text to save
+
+    Returns:
+        saved: bool
+          An indicator showing whether the text has been saved
+    """
+    saved = True
+    try:
+        with open(path_to_save, 'w', encoding='utf-8') as f:
+            f.write(text)
+    except FileNotFoundError:
+        saved = False
+    finally:
+        return saved
+
+
+def save_bytes(path_to_save: str, data: bytes) -> bool:
+    """
+    This function saves the data in bytes format in the specified path,
+    it can be the encrypted text or the encrypted symmetric key.
+
+    Parameters:
+        path_to_save: str
+          The path to save the data
+        data: bytes
+          The data to save
+
+    Returns:
+        saved: bool
+          An indicator showing whether the data has been saved
+    """
+    saved = True
+    try:
+        with open(path_to_save, 'wb') as f:
+            f.write(data)
+    except FileNotFoundError:
+        saved = False
+    finally:
+        return saved
+
+
+def save_public_key(path_to_save: str, public_key: rsa.RSAPublicKey) -> bool:
+    """
+    This function saves the public key in the specified path.
+
+    Parameters:
+        path_to_save: str
+          The path to save the key
+        public_key: rsa.RSAPublicKey
+          The key to save
+
+    Returns:
+        saved: bool
+          An indicator showing whether the key has been saved
+    """
+    saved = True
+    try:
+        with open(path_to_save, 'wb') as public_out:
+                public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                    format=serialization.PublicFormat.SubjectPublicKeyInfo))
+    except FileNotFoundError:
+        saved = False
+    finally:
+        return saved
+    
+
+def save_private_key(path_to_save:str, private_key: rsa.RSAPrivateKey) -> bool:
+    """
+    This function saves the private key in the specified path.
+
+    Parameters:
+        path_to_save: str
+          The path to save the key
+        private_key: rsa.RSAPrivateKey
+          The key to save
+
+    Returns:
+        saved: bool
+          An indicator showing whether the key has been saved
+    """
+    saved = True
+    try:
+        with open(path_to_save, 'wb') as private_out:
+            private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                encryption_algorithm=serialization.NoEncryption()))
+    except FileNotFoundError:
+        saved = False
+    finally:
+        return saved
+    
+
+def read_private_key(path_to_key: str) -> Optional[rsa.RSAPrivateKey]:
+    """
+    This function reads the private key at the specified path.
+
+    Parameters:
+        path_to_key: str
+          The path to the key
+
+    Returns:
+        key: rsa.RSAPrivateKey
+          The read key or None
+    """
+    try:
+        with open(path_to_key, 'rb') as pem_in:
+                private_bytes = pem_in.read()
+        key = load_pem_private_key(private_bytes,password=None)
+    except FileNotFoundError:
+        key = None
+    finally:
+        return key
+     
--- lab_3/main.py
+++ lab_3/main.py
@@ -1,20 +1,25 @@
 import argparse
 import os
-import json
+import sys
+
+from typing import Tuple
 
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric.padding import OAEP, MGF1
 from cryptography.hazmat.primitives.padding import ANSIX923
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
+import files
+
+
 class Cryptosystem:
-    def __init__(self, mode):
-        self.mode = mode
-    def generate_keys(self, path_to_sym, path_to_public, path_to_private, key_length):
-        symmetric_key = os.urandom(key_length)#5-16 байт
+    def __init__(self):
+        pass
+    def generate_keys(self, key_length) -> Tuple[bytes, rsa.RSAPublicKey, rsa.RSAPrivateKey]:
+        symmetric_key = os.urandom(key_length)
         keys = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048
@@ -23,52 +28,32 @@ class Cryptosystem:
         public_key = keys.public_key()
         enc_symmetric = public_key.encrypt(symmetric_key, OAEP(mgf=
                                             MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
-        with open(path_to_sym, 'wb') as f:
-            f.write(enc_symmetric)
-        with open(path_to_public, 'wb') as public_out:
-            public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
-                                format=serialization.PublicFormat.SubjectPublicKeyInfo))
-        with open(path_to_private, 'wb') as private_out:
-            private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
-                                format=serialization.PrivateFormat.TraditionalOpenSSL,
-                                encryption_algorithm=serialization.NoEncryption()))
-            
-    def encrypt_text(self, path_to_text, path_to_private, path_to_symmetric, path_to_encrypted, path_to_decrypted):
-        with open(path_to_private, 'rb') as pem_in:
-            private_bytes = pem_in.read()
-        private_key = load_pem_private_key(private_bytes,password=None)
+        return enc_symmetric, private_key, public_key
+    
 
-        with open(path_to_symmetric, 'rb') as f:
-            encrypted_sym = f.read()
-
+    def encrypt_text(self, text, encrypted_sym, private_key):
         symmetric_key = private_key.decrypt(encrypted_sym,OAEP(mgf=MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
-        len_key = len(symmetric_key)
-        with open(path_to_text, 'r', encoding='utf-8') as f:
-            text = f.read()
-        
         padder = ANSIX923(128).padder()
         b_text = bytes(text, 'UTF-8')
         padded_text = padder.update(b_text)+padder.finalize()
-
         iv = os.urandom(8)
         cipher = Cipher(algorithms.CAST5(symmetric_key), modes.CBC(iv))
         encryptor = cipher.encryptor()
         encrypted_text = encryptor.update(padded_text) + encryptor.finalize()
-
-        with open(path_to_encrypted, 'wb') as f:
-            f.write(encrypted_text)
-
-        decryptor = cipher.decryptor()
-        decrypted_text = decryptor.update(encrypted_text) + decryptor.finalize()
-
-        unpadder = ANSIX923(128).unpadder()
-        unpadded_decrypted_text = unpadder.update(decrypted_text) + unpadder.finalize()
-        final_text = unpadded_decrypted_text.decode('UTF-8')
-
-        with open(path_to_decrypted, 'w', encoding='utf-8') as f:
-            f.write(final_text)
+        return encrypted_text
         
 
+    def decrypt_text(self, encrypted_text, encrypted_sym, private_key):
+        symmetric_key = private_key.decrypt(encrypted_sym,OAEP(mgf=MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+        iv = os.urandom(8)
+        cipher = Cipher(algorithms.CAST5(symmetric_key), modes.CBC(iv))
+        decryptor = cipher.decryptor()
+        decrypted_text = decryptor.update(encrypted_text) + decryptor.finalize()
+        unpadder = ANSIX923(128).unpadder()
+        unpadded_decrypted_text = unpadder.update(decrypted_text) + unpadder.finalize()
+        final_text = unpadded_decrypted_text.decode('utf-8', errors='ignore')
+        return final_text
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Гибридная криптосистема')
@@ -77,22 +62,67 @@ if __name__ == '__main__':
     group.add_argument('-enc', '--encryption', help = 'Шифрование', action = 'store_true')
     group.add_argument('-dec', '--decryption', help = 'Дешифрование', action = 'store_true')
     parser.add_argument('settings', type = str, help = 'File with settings')
-    parser.add_argument('key_length', type = int, help = 'Key length for symmetric encryption')
+    parser.add_argument('key_length', type = int, help = 'Key length for symmetric \
+                        encryption CAST5, 40 to 128 bits (5 to 16 bytes) in length \
+                        in increments of 8 bits(5, 6, 7, 8, ... 15, 16)')
     args = parser.parse_args()
-    
-    if args.generation is not None:
-        with open(args.settings, 'r') as json_file:
-            settings = json.load(json_file)
+    if args.generation:
+        settings = files.read_json(args.settings)
+        if settings is None:
+            print("Файл с параметрами не найден")
+            sys.exit()
         key_length = args.key_length
-        enc = Cryptosystem(mode = 'gen')
-        enc.generate_keys(settings['symmetric_key'], settings['public_key'], settings['private_key'], key_length)
-        print('Успешно!')
-    
-    elif args.encryption is not None:
-        with open(args.settings, 'r') as json_file:
-            settings = json.load(json_file)
-        enc=Cryptosystem(mode = 'enc')
-        enc.encrypt_text(settings['source_text'], settings['private_key'], settings['symmetric_key'], settings['encrypted_text'], settings['decrypted_text'])
-        print('Успешно!')
+        if key_length < 5 or key_length > 16:
+            print("Неподходящий размер ключа симметричного шифрования")
+            sys.exit()
+        system = Cryptosystem()
+        symmetric, private, public = system.generate_keys(key_length)
+        path_to_symmetric = settings.get('symmetric_key')
+        path_to_public = settings.get('public_key')
+        path_to_private = settings.get('private_key')
+        if not path_to_symmetric or not path_to_public or not path_to_private:
+            print('Пути для сохранения не найдены, проверьте имена параметров')
+            sys.exit()
+        files.save_bytes(path_to_symmetric, symmetric) 
+        files.save_public_key(path_to_public, public)
+        files.save_private_key(path_to_private, private)
+        print("Генерация и сохранение ключей успешно завершены")
+    elif args.encryption:
+        settings = files.read_json(args.settings)
+        if settings is None:
+            print("Файл с параметрами не найден")
+            sys.exit()
+        system=Cryptosystem()
+        path_to_text = settings.get('source_text')
+        path_to_private = settings.get('private_key')
+        path_to_symmetric = settings.get('symmetric_key')
+        path_to_encrypted = settings.get('encrypted_text')
+        if not path_to_text or not path_to_private or not path_to_symmetric or not path_to_encrypted:
+            print('Пути не найдены, проверьте имена параметров')
+            sys.exit()
+        text = files.read_text(path_to_text)
+        private = files.read_private_key(path_to_private)
+        symmetric = files.read_bytes(path_to_symmetric)
+        encrypted_text = system.encrypt_text(text, symmetric, private)
+        files.save_bytes(path_to_encrypted, encrypted_text)
+        print('Шифрование и сохранение текста выполнены успешно')
     else:
-        print(f'Дешифрование {args.settings}')
+        settings = files.read_json(args.settings)
+        if settings is None:
+            print("Файл с параметрами не найден")
+            sys.exit()
+        system=Cryptosystem()
+        path_to_encrypted = settings.get('encrypted_text')
+        path_to_private = settings.get('private_key')
+        path_to_symmetric = settings.get('symmetric_key')
+        path_to_decrypted = settings.get('decrypted_text')
+        if not path_to_encrypted or not path_to_private or not path_to_symmetric or not path_to_decrypted:
+            print('Пути не найдены, проверьте имена параметров')
+            sys.exit()
+        encrypted_text = files.read_bytes(path_to_encrypted)
+        private = files.read_private_key(path_to_private)
+        symmetric = files.read_bytes(path_to_symmetric)
+        decrypted_text = system.decrypt_text(encrypted_text, symmetric, private)
+        files.save_text(path_to_decrypted, decrypted_text)
+        print('Расшифровка и сохранение выполнены успешно')
+         
